5983d8d83148e2b9304d21308378ba46b42214bf
Daniel <romeroibarradaniel@gmail.com>
2024-10-05 15:00:51 -0600

    Archivos agregados

---
+++ app.js
@@ -0,0 +1,32 @@
+
+function crearTarea() {
+    const tarea = document.getElementById("tarea").value.trim();
+    const lista = document.getElementById("lista");
+    const elemento = document.createElement("li");
+    const botonEliminar = document.createElement("button");
+    const vaciar = document.getElementById("tarea");
+
+    if (tarea === "") {
+        alert("Ingresa una tarea");
+        return;
+    }
+
+    elemento.textContent = tarea;
+    elemento.title = "Click para marcar como completado";
+
+    elemento.addEventListener('click', function () {
+        elemento.classList.toggle('completada');
+    });
+
+
+    botonEliminar.textContent = "Eliminar";
+
+    botonEliminar.addEventListener('click', function () {
+        elemento.remove()
+    });
+
+    elemento.appendChild(botonEliminar);
+    lista.appendChild(elemento);
+
+    vaciar.value = "";
+}
